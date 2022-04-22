83f42f3164f98857ea23a3933257de0d5638d2a4
Quixelation <58915890+Quixelation@users.noreply.github.com>
2022-04-22 20:57:05 +0200

    last resort with custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html' if (kIsWeb) '';
-
 import 'package:anger_buddy/database.dart';
 import 'package:anger_buddy/logic/aushang/aushang.dart';
 import 'package:anger_buddy/logic/color_manager/color_manager.dart';
@@ -101,12 +99,7 @@ class _MyAppState extends State<MyApp> {
                     brightness: Brightness.dark))
             .copyWith(
                 textTheme: Theme.of(context).textTheme.apply(
-                      fontFamily: kIsWeb &&
-                              (window?.navigator?.userAgent
-                                      ?.contains('OS 15_') ??
-                                  false)
-                          ? '-apple-system'
-                          : null,
+                      fontFamily: kIsWeb ? 'Montserrat' : null,
                     ),
                 drawerTheme:
                     const DrawerThemeData(backgroundColor: Color(0xFF232323))),
