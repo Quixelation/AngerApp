b396c813eef3681392a83a2a1881b2cc889524a6
Robert <58915890+Quixelation@users.noreply.github.com>
2022-12-02 21:08:06 +0100

    update mapWithIndex to provide types
    need to be refactored

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,5 +1,5 @@
 extension MapWithIndex<E> on List {
-  List<T> mapWithIndex<T>(T Function(E element, int index) cb) {
+  List<T> mapWithIndex<T, E>(T Function(E element, int index) cb) {
     List<T> mapList = [];
     for (var i = 0; i < this.length; i++) {
       mapList.add(cb(this[i], i));
