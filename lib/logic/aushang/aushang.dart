e53773d619581f6f16d8053490017bbffdcda82a
Robert <58915890+Quixelation@users.noreply.github.com>
2022-12-02 22:08:51 +0100

    identation fix

--- lib/logic/aushang/aushang.dart
+++ lib/logic/aushang/aushang.dart
@@ -35,7 +35,8 @@ String _createAuthHeader() {
   if (Credentials.vertretungsplan.credentialsAvailable) {
     return "Bearer " + (Credentials.vertretungsplan.subject.value ?? "");
   } else {
-    logger.e("No VP Creds available, but requested for AuthHeader", null, StackTrace.current);
+    logger.e("No VP Creds available, but requested for AuthHeader", null,
+        StackTrace.current);
     throw ErrorDescription("No VP Creds available");
   }
 }
@@ -83,8 +84,12 @@ class Aushang {
     dateCreated = DateTime.parse(map["dateCreated"]);
     dateUpdated = DateTime.parse(map["dateUpdated"]);
     textContent = map["textContent"];
-    files = (map["files"] as List<dynamic>).map((e) => int.parse(e.toString())).toList();
-    klassenstufen = (map["klassenstufen"] as List<dynamic>? ?? []).map((e) => int.parse(e.toString())).toList();
+    files = (map["files"] as List<dynamic>)
+        .map((e) => int.parse(e.toString()))
+        .toList();
+    klassenstufen = (map["klassenstufen"] as List<dynamic>? ?? [])
+        .map((e) => int.parse(e.toString()))
+        .toList();
     fixed = map["fixed"];
   }
 
@@ -93,7 +98,9 @@ class Aushang {
 
     final db = getIt.get<AppManager>().db;
     if (state == ReadStatusBasic.read) {
-      await AppManager.stores.aushaengeLastRead.record(id).put(db, {"datetime": DateTime.now().millisecondsSinceEpoch});
+      await AppManager.stores.aushaengeLastRead
+          .record(id)
+          .put(db, {"datetime": DateTime.now().millisecondsSinceEpoch});
     } else if (state == ReadStatusBasic.notRead) {
       await AppManager.stores.aushaengeLastRead.record(id).delete(db);
     } else {
@@ -108,10 +115,12 @@ class Aushang {
       // load Data from Subject
       var tempAushangValue = subject.value;
       // check that it's not empty
-      if (tempAushangValue == null) throw ErrorDescription("aushangSubject is emtpy somehow...");
+      if (tempAushangValue == null)
+        throw ErrorDescription("aushangSubject is emtpy somehow...");
 
       // get the index of the value to edit
-      var elemIndex = tempAushangValue.indexWhere((element) => element.uniqueId == id);
+      var elemIndex =
+          tempAushangValue.indexWhere((element) => element.uniqueId == id);
       // edit the value
       tempAushangValue[elemIndex].read = state;
 
@@ -121,12 +130,15 @@ class Aushang {
       var subject = AngerApp.aushang.subject;
       var tempAushangValue = subject.value;
 
-      if (tempAushangValue == null) throw ErrorDescription("aushangSubject is emtpy somehow...");
+      if (tempAushangValue == null)
+        throw ErrorDescription("aushangSubject is emtpy somehow...");
 
-      var newList = tempAushangValue.data.where((element) => element.id != id).toList();
+      var newList =
+          tempAushangValue.data.where((element) => element.id != id).toList();
       newList.add(this);
 
-      var newData = AsyncDataResponse(data: newList, loadingAction: AsyncDataResponseLoadingAction.none);
+      var newData = AsyncDataResponse(
+          data: newList, loadingAction: AsyncDataResponseLoadingAction.none);
 
       Services.aushang.subject.add(newData);
     }
@@ -154,22 +166,30 @@ class _AushangFile {
 Future<_AushangFile> __loadFile(int fileId) async {
   try {
     print("LOADINGFILE $fileId");
-    var fileIdResult = await http.get(Uri.parse("${AppManager.directusUrl}/items/aushang_files/$fileId"), headers: {
-      "Authorization": _createAuthHeader(),
-    });
+    var fileIdResult = await http.get(
+        Uri.parse("${AppManager.directusUrl}/items/aushang_files/$fileId"),
+        headers: {
+          "Authorization": _createAuthHeader(),
+        });
     //TODO: Add Check for Status-Code etc.
     var fileIdResultJson = jsonDecode(fileIdResult.body);
     var directusFilesId = fileIdResultJson["data"]["directus_files_id"];
 
-    var directusFileResult = await http.get(Uri.parse("${AppManager.directusUrl}/files/$directusFilesId"), headers: {
-      "Authorization": _createAuthHeader(),
-    });
+    var directusFileResult = await http.get(
+        Uri.parse("${AppManager.directusUrl}/files/$directusFilesId"),
+        headers: {
+          "Authorization": _createAuthHeader(),
+        });
     //s.a.: add statuscode check
     var directusFileResultJson = jsonDecode(directusFileResult.body);
     logger.d(directusFileResultJson);
     var fileName = directusFileResultJson["data"]["title"];
     var fileType = directusFileResultJson["data"]["type"];
-    return _AushangFile(title: fileName, directusFileId: directusFilesId, fileId: fileId, type: fileType);
+    return _AushangFile(
+        title: fileName,
+        directusFileId: directusFilesId,
+        fileId: fileId,
+        type: fileType);
   } catch (e) {
     logger.e("Error Loading File");
     logger.e(e);
@@ -190,21 +210,26 @@ Future<List<_AushangFile>> _loadFiles(Aushang aushang) async {
 }
 
 /// Bestimmt, ob es eine neue bzw. ungelesene Version des Aushangs gibt
-Future<ReadStatusBasic> getAushangReadStatusFromDatabase(String aushangId, DateTime newLastRead) async {
+Future<ReadStatusBasic> getAushangReadStatusFromDatabase(
+    String aushangId, DateTime newLastRead) async {
   try {
     final db = getIt.get<AppManager>().db;
-    final entryExists = await AppManager.stores.aushaengeLastRead.record(aushangId).exists(db);
+    final entryExists =
+        await AppManager.stores.aushaengeLastRead.record(aushangId).exists(db);
 
     if (!entryExists) {
       logger.v("[Aushang] Aushang doesn't exist in ReadStatusDB");
       return ReadStatusBasic.notRead;
     }
 
-    final dbEntry = await AppManager.stores.aushaengeLastRead.record(aushangId).get(db);
+    final dbEntry =
+        await AppManager.stores.aushaengeLastRead.record(aushangId).get(db);
 
-    logger.d("parsed Int " + int.parse(dbEntry!["datetime"].toString()).toString());
+    logger.d(
+        "parsed Int " + int.parse(dbEntry!["datetime"].toString()).toString());
     logger.d("NewLastRd: " + newLastRead.millisecondsSinceEpoch.toString());
-    return (int.parse(dbEntry["datetime"].toString())) < newLastRead.millisecondsSinceEpoch
+    return (int.parse(dbEntry["datetime"].toString())) <
+            newLastRead.millisecondsSinceEpoch
         ? ReadStatusBasic.notRead
         : ReadStatusBasic.read;
   } catch (err) {
@@ -216,6 +241,7 @@ Future<void> DEVONLYdeleteAushangReadStateForAllAushange() async {
   try {
     final db = getIt.get<AppManager>().db;
     final entryExists = await AppManager.stores.aushaengeLastRead.delete(db);
+    logger.i("Aushänge gelöscht");
   } catch (err) {
     logger.e(err);
   }
@@ -226,7 +252,8 @@ class AushangManager extends DataManager<Aushang> {
 
   ///# Only update/add with `updateSubject()`
   final subject = BehaviorSubject();
-  BehaviorSubject<List<VpAushang>> vpAushangSubject = BehaviorSubject<List<VpAushang>>();
+  BehaviorSubject<List<VpAushang>> vpAushangSubject =
+      BehaviorSubject<List<VpAushang>>();
 
   @override
   String get syncManagerKey => "aushang";
@@ -236,11 +263,13 @@ class AushangManager extends DataManager<Aushang> {
     var result = await retryManager<http.Response>(function: () async {
       final authHeader = _createAuthHeader();
       logger.v("authHeader: $authHeader");
-      var result = await http.get(Uri.parse("${AppManager.directusUrl}/items/aushang/"), headers: {
+      var result = await http
+          .get(Uri.parse("${AppManager.directusUrl}/items/aushang/"), headers: {
         "Authorization": authHeader,
       });
       if (result.statusCode != 200) {
-        logger.v("[AushangManager] StatusCode failed with ${result.statusCode}");
+        logger
+            .v("[AushangManager] StatusCode failed with ${result.statusCode}");
         logger.v("[AushangManager] ${result.body}");
 
         throw Error();
@@ -257,7 +286,8 @@ class AushangManager extends DataManager<Aushang> {
         final dateUpdated = aushang["date_updated"] != null
             ? DateTime.parse(aushang["date_updated"])
             : DateTime.fromMicrosecondsSinceEpoch(0);
-        final readStatus = await getAushangReadStatusFromDatabase(aushangId, dateUpdated);
+        final readStatus =
+            await getAushangReadStatusFromDatabase(aushangId, dateUpdated);
 
         aushaengeList.add(Aushang(
             id: aushangId,
@@ -266,11 +296,14 @@ class AushangManager extends DataManager<Aushang> {
             dateCreated: DateTime.parse(aushang["date_created"]),
             dateUpdated: dateUpdated,
             textContent: aushang["textContent"].toString(),
-            files: (aushang["files"] as List<dynamic>).map((e) => int.parse(e.toString())).toList(),
+            files: (aushang["files"] as List<dynamic>)
+                .map((e) => int.parse(e.toString()))
+                .toList(),
             read: readStatus,
             fixed: aushang["fixed"] == true,
-            klassenstufen:
-                ((aushang["klassen"] as List<dynamic>?) ?? []).map((e) => int.parse(e.toString())).toList()));
+            klassenstufen: ((aushang["klassen"] as List<dynamic>?) ?? [])
+                .map((e) => int.parse(e.toString()))
+                .toList()));
       } catch (e) {
         logger.d(aushang);
         logger.e(e);
@@ -289,7 +322,9 @@ class AushangManager extends DataManager<Aushang> {
     await db.transaction((transaction) async {
       await AppManager.stores.aushaenge.delete(transaction);
       for (var aushang in aushange) {
-        await AppManager.stores.aushaenge.record(aushang.id).put(transaction, aushang.toMap());
+        await AppManager.stores.aushaenge
+            .record(aushang.id)
+            .put(transaction, aushang.toMap());
       }
     });
     logger.v("[Aushang] Saved into Database");
@@ -302,7 +337,8 @@ class AushangManager extends DataManager<Aushang> {
     var db = getIt.get<AppManager>().db;
 
     var snapshots = await AppManager.stores.aushaenge.query().getSnapshots(db);
-    logger.d("[Aushang] DB-count: ${await AppManager.stores.aushaenge.count(db)}");
+    logger.d(
+        "[Aushang] DB-count: ${await AppManager.stores.aushaenge.count(db)}");
     logger.d("[Aushang] snapshot-length: ${snapshots.length}");
     List<Aushang> aushangList = [];
 
@@ -310,7 +346,8 @@ class AushangManager extends DataManager<Aushang> {
       final dateUpdated = snapshot["dateUpdated"] != null
           ? DateTime.parse(snapshot["dateUpdated"]!.toString())
           : DateTime.fromMicrosecondsSinceEpoch(0);
-      final readStatus = await getAushangReadStatusFromDatabase(snapshot.key, dateUpdated);
+      final readStatus =
+          await getAushangReadStatusFromDatabase(snapshot.key, dateUpdated);
       aushangList.add(Aushang.fromMap(snapshot.value, read: readStatus));
     }
 
