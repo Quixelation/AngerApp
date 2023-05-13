9723683aa303c8db8f5370b4d1824319ccdf42d5
Robert <58915890+Quixelation@users.noreply.github.com>
2023-05-13 16:09:36 +0200

    better creds input vp

--- lib/logic/vertretungsplan/page_creds.dart
+++ lib/logic/vertretungsplan/page_creds.dart
@@ -20,8 +20,7 @@ class _PageVpCredsState extends State<_PageVpCreds> {
           width: 300,
           child: Form(
             key: _formKey,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               TextFormField(
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -29,11 +28,10 @@ class _PageVpCredsState extends State<_PageVpCreds> {
                   }
                   return null;
                 },
+                keyboardType: TextInputType.phone,
                 controller: passwordController,
-                decoration: const InputDecoration(
-                    labelText: "Kennwort",
-                    helperText: "Das gleiche Kennwort wie auf Newspoint",
-                    icon: Icon(Icons.password)),
+                decoration:
+                    const InputDecoration(labelText: "Kennwort", helperText: "Das gleiche Kennwort wie auf Newspoint", icon: Icon(Icons.password)),
               ),
               const SizedBox(
                 height: 35,
@@ -41,8 +39,7 @@ class _PageVpCredsState extends State<_PageVpCreds> {
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
-                    Credentials.vertretungsplan
-                        .setCredentials(passwordController.text.trim());
+                    Credentials.vertretungsplan.setCredentials(passwordController.text.trim());
                   }
                 },
                 child: const Text("Verifizieren"),
@@ -53,8 +50,7 @@ class _PageVpCredsState extends State<_PageVpCreds> {
         const Positioned(
           child: Opacity(
               opacity: 0.6,
-              child: Text(
-                  "Auch wenn andere Kennwörter funktionieren, wird nur das offizielle Kennwort der Schule unterstützt.",
+              child: Text("Auch wenn andere Kennwörter funktionieren, wird nur das offizielle Kennwort der Schule unterstützt.",
                   textAlign: TextAlign.center)),
           left: 15,
           right: 15,
